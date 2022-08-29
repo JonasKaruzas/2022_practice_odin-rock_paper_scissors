ddf9a0bcf51187f2a10b2374e17fdb2e0950a621
JonasKaruzas <jonas.karuzas@gmail.com>
2022-08-29 23:52:36 +0300

    feat: :poop: show and clean selection

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ let playerScore = 0;
 let computerScore = 0;
 let gameOver = false;
 
-rockBtn.addEventListener("click", () => {
-  playRound("rock", getComputerChoice(objects));
+rockBtn.addEventListener("click", (e) => {
+  playRound("rock", getComputerChoice(objects), e);
 });
 
-paperBtn.addEventListener("click", () => {
-  playRound("paper", getComputerChoice(objects));
+paperBtn.addEventListener("click", (e) => {
+  playRound("paper", getComputerChoice(objects), e);
 });
 
-scissorBtn.addEventListener("click", () => {
-  playRound("scissor", getComputerChoice(objects));
+scissorBtn.addEventListener("click", (e) => {
+  playRound("scissor", getComputerChoice(objects), e);
 });
 
 resetBtn.addEventListener("click", () => {
@@ -38,21 +38,34 @@ function getComputerChoice(objects) {
   return objects[getRandomInt(0, objects.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerScore++;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++;
-  } else {
-    computerScore++;
-  }
+function playRound(playerSelection, computerSelection, event) {
+  showPlayerSelection(event);
 
-  updateScore();
-  isGameOver();
+  setTimeout(() => {
+    showComputerSelection(computerSelection);
+    checkWhoWon();
+  }, 800);
+
+  function checkWhoWon() {
+    if (playerSelection === computerSelection) {
+      return;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      playerScore++;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      playerScore++;
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      playerScore++;
+    } else {
+      computerScore++;
+    }
+    updateScore();
+    removeSelections();
+
+    isGameOver();
+  }
 }
 
 function updateScore() {
@@ -71,4 +84,21 @@ function resetGame() {
   playerScore = 0;
   computerScore = 0;
   updateScore();
+}
+
+function showPlayerSelection(event) {
+  console.dir(event.target);
+  event.target.classList.add("playerActive");
+}
+
+function showComputerSelection(target) {
+  const element = "." + target;
+  const card = document.querySelector(element);
+  card.classList.add("computerActive");
+}
+
+function removeSelections() {
+  rockBtn.classList.remove("playerActive", "computerActive");
+  paperBtn.classList.remove("playerActive", "computerActive");
+  scissorBtn.classList.remove("playerActive", "computerActive");
 }
