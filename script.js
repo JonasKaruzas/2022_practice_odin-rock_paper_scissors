d65358ee53a78228d904ff0b871a9653ec82aba2
JonasKaruzas <jonas.karuzas@gmail.com>
2022-08-29 19:55:19 +0300

    feat: :sparkles: getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("let's start");
+const objects = ["rock", "paper", "scissors"];
+
+function getComputerChoice(objects) {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  return objects[getRandomInt(0, objects.length)];
+}
+
+console.log(getComputerChoice(objects));
